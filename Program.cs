18ef1f5b32b1304e34f98fb16cca4c5851e45e6f
Artem Bandura <153463378+Artem-neloshka@users.noreply.github.com>
2024-04-02 21:09:08 +0300

    add files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿/*5 kyu
+I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it?
+According to Wikipedia, ROT13 is frequently used to obfuscate jokes on USENET.
+
+For this task you're only supposed to substitute characters. Not spaces, punctuation, numbers, etc.
+
+Test examples:
+
+"EBG13 rknzcyr." -> "ROT13 example."
+
+"This is my first ROT13 excercise!" -> "Guvf vf zl svefg EBG13 rkprepvfr!"*/
+
